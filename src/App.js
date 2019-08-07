9100c71956e84ee8450d5884ac782071dcc2a4b2
Priskila Ritonga <ritongapriskila@gmail.com>
2019-08-07 12:18:51 +0700

    add rank

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 
 import Navigation from './components/Navigation';
 import Logo from './components/Logo';
 import ImageLinkForm from './components/ImageLinkForm';
+import Rank from './components/Rank'
 
 
 
@@ -10,16 +12,33 @@ import ImageLinkForm from './components/ImageLinkForm';
 
 import './App.css';
 
+
+const particles = { //parctiles for background
+  articles: {
+    number: {
+      value: 104,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particles} />
 
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
+
       {/* <FaceRecognition /> */}
 
-      
+
     </div>
   );
 }
---
+++ src/components/Rank.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Rank=()=>{
+    return(
+        <div>
+            <div className='white f3'>
+                your current rank is ..... 
+                #1
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
