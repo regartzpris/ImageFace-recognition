2f241335a88f35a7e5bb382f78e587e424b2a3dd
Priskila Ritonga <ritongapriskila@gmail.com>
2019-08-09 02:32:21 +0700

    fix bugs before deploy to heroku

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -8,7 +8,7 @@ const SignIn = ({onRouteChange}) => {
                 <div className="measure">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                     
-                        <legend className="f4 fw6 ph0 mh0"> Please clik Sign In to enter App</legend>
+                        <legend className="f4 fw6 ph0 mh0">Please clik Sign In to enter App</legend>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                             <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
